fae022c90596f1fa7e626583c91077198003e9a8
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-08-23 12:10:36 +0500

    Updated

--- Lottery_System.sol
+++ Lottery_System.sol
@@ -13,12 +13,22 @@ contract LotterSystem {
 
      receive() payable external {
  
+        require(msg.value== 2 ether , "Entry Fee Must Be 2 Ethers");
         participants.push(payable(msg.sender));
          
      }
 
      function Check_Balance() public view returns(uint){
+         require(msg.sender==manager, "Not Allowed");
          return address(this).balance; 
+     }
+
+
+     function GenerateRandom() public view returns(uint){
+
+
+       return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,participants.length)));
+
      }
     
 }

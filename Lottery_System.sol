86426bb4e3bd786d8e0ca9fb97ff098efb1761c2
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-08-23 11:09:25 +0500

    Updated

--- Lottery_System.sol
+++ Lottery_System.sol
@@ -1,29 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
+pragma solidity 0.8.21;
 
-contract day6 {
-  //  function average(int a,int b,int c) public pure returns(int){
+contract LotterSystem {
+
+    address payable public manager;
+    address payable[] public participants;
+
+     constructor() {
+         manager=payable(msg.sender);
+     }
+
+
+     receive() payable external {
+ 
+        participants.push(payable(msg.sender));
+         
+     }
+
+     function Check_Balance() public view returns(uint){
+         return address(this).balance; 
+     }
     
-  //    int av = a+b+c;
-  //    av = av/3;
-  //    return av;
-  //  }
-
-
-   function digitsum(int n) public pure returns(int){
-      
-      require(n >0 ,"value should be greater than zero");
-
-      int sum = 0;
-      
-      while(n>0){
-       sum += n % 10;
-       n = n/10;
-
-      }
-
-      return sum;
-
-    }
-
 }
